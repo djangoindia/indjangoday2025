faa047af3a480e671e1cf7028511fd02a9829b9f
Saurabh <saurabh@192.168.1.6>
2025-02-23 19:04:29 +0530

    Landing Page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: "build",
+  redirects: async () => [
+    {
+      source: "/(.*)",
+      destination: "/",
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
